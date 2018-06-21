48b76eb036c9edeee47fa6a0ae5390403792abc7
Byron Warner <bfwarner@gmail.com>
2018-06-20 22:52:37 -0700

    bundled code with webpack 4

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+// rollup.config.js
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+//import external-helpers from 'external-helpers';
+import babelrc from 'babelrc-rollup';
+
+
+export default {
+  input: 'lib/index.js',
+  output: {
+    name: 'emotion-react-bootstrap',
+    file: 'bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    resolve({
+      // use 'module' field for ES6 module if possible
+      //module: true, // Default: true
+
+      // use 'jsnext:main' if possible
+      // – see https://github.com/rollup/rollup/wiki/jsnext:main
+      //jsnext: true,  // Default: false
+
+
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      //preferBuiltins: false,  // Default: true
+
+
+      // Set to an array of strings and/or regexps to lock the module search
+      // to modules that match at least one entry. Modules not matching any
+      // entry will be marked as external
+      //only: [ 'some_module', /^@some_scope\/.*$/ ], // Default: null
+
+      // If true, inspect resolved files to check that they are
+      // ES2015 modules
+      //modulesOnly: true, // Default: false
+
+      // Any additional options that should be passed through
+      // to node-resolve
+      //customResolveOptions: {
+      //  moduleDirectory: 'js_modules'
+      //}
+    }),
+    babel(babelrc),
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const config = {
+  entry: './lib/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2',
+    library: 'emotion-react-bootstrap',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+    ],
+  },
+};
+
+
+const config2 = {
+  entry: './lib/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.web.js',
+  },
+  module: {
+    rules: [
+      { test: /.js$/, use: 'babel-loader' },
+    ],
+  },
+};
+
+module.exports = [
+  (env, argv) => {
+    const mods = { target: 'web' };
+    if (argv.mode === 'production') {
+      mods.mode = 'production';
+    } else {
+      mods.devtool = 'source-map';
+    }
+    return { ...config2, ...mods };
+  },
+  (env, argv) => {
+    const mods = {
+      target: 'node'
+    };
+    if (argv.mode === 'production') {
+      mods.mode = 'production';
+    } else {
+      mods.devtool = 'source-map';
+    }
+    return { ...config, ...mods };
+  },
+];
