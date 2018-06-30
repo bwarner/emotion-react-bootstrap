6dbf4fa709d69b5ea592d90e4d114d7cbc1b1e3e
Benny Anderson <hafabenny@gmail.com>
2018-06-30 16:02:06 -0700

    initial work on alert component

---
+++ stories/alert.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Alert from '../lib/alert';
+
+export default function story() {
+  return (
+    <Alert className="alert" color="primary">
+      Hello from alert stories
+    </Alert>
+  );
+}
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,7 +1,10 @@
 import { storiesOf } from '@storybook/react';
 import form from './form.stories';
 import layout from './layout.stories';
+import alert from './alert.stories';
 
 storiesOf('Form', module)
   .add('Simple Form', form)
   .add('Layout Components', layout);
+
+storiesOf('Alert', module).add('Simple Alert', alert);
