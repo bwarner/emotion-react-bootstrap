cf851d42d157330e9229b71b009a38dc3941314d
Benny Anderson <hafabenny@gmail.com>
2018-06-30 20:05:37 -0700

    more implementation of alert component

--- stories/alert.stories.js
+++ stories/alert.stories.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Alert from '../lib/alert';
 
-export default function story() {
-  return (
-    <Alert className="alert" color="primary">
-      Hello from alert stories
-    </Alert>
-  );
-}
+const SimpleAlertStory = () => (
+  <Alert className="alert">
+    This is a primary alert with
+    <a href="http://www.healthline.com" className="alert-link">
+      an example link
+    </a>. Give it a click if you like.
+  </Alert>
+);
+
+const SuccessAlertStory = () => (
+  <Alert className="alert" color="success">
+    Hello from alert stories
+  </Alert>
+);
+
+export { SimpleAlertStory, SuccessAlertStory };
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,12 @@
 import { storiesOf } from '@storybook/react';
 import form from './form.stories';
 import layout from './layout.stories';
-import alert from './alert.stories';
+import { SimpleAlertStory, SuccessAlertStory } from './alert.stories';
 
 storiesOf('Form', module)
   .add('Simple Form', form)
   .add('Layout Components', layout);
 
-storiesOf('Alert', module).add('Simple Alert', alert);
+storiesOf('Alert', module)
+  .add('Simple Alert', SimpleAlertStory)
+  .add('Success Alert', SuccessAlertStory);
