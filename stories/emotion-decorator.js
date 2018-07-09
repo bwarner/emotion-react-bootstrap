54055d62450b483f0604db03e3102277cf7e32e7
Byron Warner <bfwarner@gmail.com>
2018-07-08 20:17:54 -0700

    remove unneeded extractCritcal and fixed active class in button component

--- stories/emotion-decorator.js
+++ stories/emotion-decorator.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { extractCritical } from 'emotion-server';
-import { renderToString } from 'react-dom/server';
 
 export default function decorator(story) {
-  const { css } = extractCritical(renderToString(story()));
 
   return (
     <div>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: css,
-        }}
-      />
       <div
         style={{
           height: '100%',
