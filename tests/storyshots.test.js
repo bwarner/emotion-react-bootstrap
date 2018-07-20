bc039c6629c93d0d535bf7e6c4c92dbea7aec1fb
Byron Warner <bfwarner@gmail.com>
2018-07-20 02:32:34 -0700

    Inital work on storybook:build (#21)
    * uses scoped emotion-bootstrap module
    
    ^ Supports Image Snapshots
    
    * upload storybook

--- tests/storyshots.test.js
+++ tests/storyshots.test.js
@@ -1,5 +1,13 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots({
-  /* configuration options */
-});
+if (process.env.CI === 'true') {
+  initStoryshots({
+    suite: 'Image storyshots',
+    test: imageSnapshot({ storybookUrl: `file://${process.env.CIRCLE_WORKING_DIRECTORY}/out/static` }),
+  });
+} else {
+  initStoryshots({
+    suite: 'Image storyshots',
+  });
+}
